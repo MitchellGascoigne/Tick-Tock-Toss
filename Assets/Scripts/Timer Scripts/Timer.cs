e221479f57010449205e49a333d6bbed2080ef43
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-10-23 17:52:32 +1100

    Script Edits

--- Assets/Scripts/Timer Scripts/Timer.cs
+++ Assets/Scripts/Timer Scripts/Timer.cs
@@ -1,30 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
 public class Timer : MonoBehaviour
 {
-    public float countdown = 10;
-    public TextMeshProUGUI countText;
-    private bool active = true;
+    private float countdown = 10f; // Initialize a countdown timer with a value of 10 seconds.
+    public TMP_Text countNumbers; // Reference to a TextMeshPro text component for displaying the countdown.
+
+    void Start()
+    {
+        if (countNumbers == null)
+        {
+            Debug.LogError("countNumbers is not assigned. Please assign it in the Inspector.");
+            enabled = false; // Disable the script to prevent further errors.
+        }
+    }
 
     void Update()
     {
-        if (active)
+        if (countNumbers != null)
         {
-            countdown -= Time.deltaTime;
-
-            if (countdown <= 0)
+            if (countdown > 0)
             {
-                PlayerController playerController = transform.parent.GetComponent<PlayerController>();
-                if (playerController != null)
-                {
-                    playerController.DestroyPlayer();
-                }
-
-                active = false;
+                countdown -= Time.deltaTime; // Decrement the countdown by the time passed in the current frame.
+            }
+            else
+            {
+                // DESTROY TIMER
+                // DESTROY PLAYER
             }
 
-            countText.text = Mathf.Ceil(countdown).ToString();
+            double b = System.Math.Round(countdown, 2); // Round the countdown value to two decimal places and store it in a double variable 'b'.
+            countNumbers.text = b.ToString(); // Update the TextMeshPro text component with the rounded countdown value as a string.
         }
     }
 }
+
