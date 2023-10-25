7aecc1cf4dd5feb29eb3a572d1247235dfdd267a
microman502 <microman502@gmail.com>
2023-10-25 14:43:07 +1100

    Replaced TimerInfo and TimerFunction with ItemInfo
    Removed TimerInfo and TimerFunction as they are unnecessary. Replaced them with ItemInfo, and updated Item to support these changes.

---
+++ Assets/Scripts/Items/ItemInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ItemData/Generic")]
+// Generic item. Works with Item and ItemBombPassable.
+public class ItemInfo : ScriptableObject
+{
+    public string itemName;
+}
