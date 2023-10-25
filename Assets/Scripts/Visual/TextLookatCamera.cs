f3764da9b1803661a90df8f22f87564807b16a1a
microman502 <microman502@gmail.com>
2023-10-25 16:39:20 +1100

    TextLookAtCamera validation check
    TextLookAtCamera.Update() now ensure that Camera.current exists before running.

--- Assets/Scripts/Visual/TextLookatCamera.cs
+++ Assets/Scripts/Visual/TextLookatCamera.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-//
 public class TextLookatCamera : MonoBehaviour
 {
     void Update ()
     {
+        if (!Camera.current)
+            return;
+
         // Set forward facing away from the camera, since the text is flipped
         transform.forward = -(Camera.current.transform.position - transform.position); 
     }
