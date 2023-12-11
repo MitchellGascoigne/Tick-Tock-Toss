fb58d83fb390f6d1e60991a56f56a13d38de3c90
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2023-12-12 10:40:46 +1100

    WebGL Build

--- Assets/Scripts/Player/PlayerGroundCheck.cs
+++ Assets/Scripts/Player/PlayerGroundCheck.cs
@@ -11,25 +11,25 @@ public class PlayerGroundCheck : MonoBehaviour
         playerController = GetComponentInParent<PlayerController>();
     }
 
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.gameObject == playerController.gameObject)
-    //        return;
-    //    playerController.SetGroundedState(true);
-    //}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(true);
+    }
 
-    //void OnTriggerExit(Collider other)
-    //{
-    //    if (other.gameObject == playerController.gameObject)
-    //        return;
-    //    playerController.SetGroundedState(false);
-    //}
-    //void OnTriggerStay(Collider other)
-    //{
-    //    if (other.gameObject == playerController.gameObject)
-    //        return;
-    //    playerController.SetGroundedState(true);
-    //}
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(false);
+    }
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(true);
+    }
 
     void OnCollisionEnter(Collision collision)
     {
@@ -43,12 +43,12 @@ public class PlayerGroundCheck : MonoBehaviour
             return;
         playerController.SetGroundedState(false);
     }
-    //void OnCollisionStay(Collision collision)
-    //{
-    //    if (collision.gameObject == playerController.gameObject)
-    //        return;
-    //    playerController.SetGroundedState(true);
-    //}
+    void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject == playerController.gameObject)
+            return;
+        playerController.SetGroundedState(true);
+    }
 
 }
 
