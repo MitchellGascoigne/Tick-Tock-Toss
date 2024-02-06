d83ee5a6909e2f66e2b5c726871b6d28ee4b887c
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2024-02-07 08:43:06 +1100

    UI clean-up
    - Imported New Window Menu

--- Assets/Audio/DontDestroyAudio.cs
+++ Assets/Audio/DontDestroyAudio.cs
@@ -1,23 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor.SearchService;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
 
-public class DontDestroyAudio : MonoBehaviour
-{
-    void Awake()
-    {
-        //DontDestroyOnLoad(transform.gameObject);
+//public class DontDestroyAudio : MonoBehaviour
+//{
+//    void Awake()
+//    {
+//        //DontDestroyOnLoad(transform.gameObject);
 
-        ////// Get the current active scene
-        ////UnityEngine.SceneManagement.Scene currentScene = SceneManager.GetActiveScene();
+//        ////// Get the current active scene
+//        ////UnityEngine.SceneManagement.Scene currentScene = SceneManager.GetActiveScene();
 
-        ////// Check if the current scene name is "Gamescene"
-        ////if (currentScene.name == "Game")
-        ////{
-        ////    Destroy(gameObject);
-        ////}
-    }
-}
+//        ////// Check if the current scene name is "Gamescene"
+//        ////if (currentScene.name == "Game")
+//        ////{
+//        ////    Destroy(gameObject);
+//        ////}
+//    }
+//}
