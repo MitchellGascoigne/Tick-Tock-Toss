284ce0edaabef68d1e44ad83b8e829fcb0a56d7f
MitchellGascoigne <107447804+MitchellGascoigne@users.noreply.github.com>
2024-02-20 08:14:28 +1100

    (Forgot to push Changes)

---
+++ Assets/Explosive.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosive : MonoBehaviour 
+{
+    public float delay = 3f;
+
+    float countdown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        countdown = delay;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        countdown -= Time.deltaTime;
+        if (countdown <= 0f)
+        {
+            Explode();
+        }
+    }
+
+    void Explode()
+    {
+        Debug.Log("BOOM!");
+    }
+}
