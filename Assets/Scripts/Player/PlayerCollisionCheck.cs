cecedfb566902db55f6d13aab700dd0238e4826b
microman502 <microman502@gmail.com>
2024-03-19 21:28:17 +1100

    PlayerCollisionCheck, primitive wall colliders
    Added PlayerCollisionCheck to prevent players from clipping through walls.
    Added box colliders to most wall prefabs.

---
+++ Assets/Scripts/Player/PlayerCollisionCheck.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionCheck : MonoBehaviour
+{
+    [SerializeField] LayerMask ignoreLayers;
+    Vector3 lastPosition;
+
+    void Start ()
+    {
+        lastPosition = transform.position;
+    }
+
+    void FixedUpdate ()
+    {
+        Vector3 currentPosition = transform.position;
+
+        // If the player has moved enough in one physics frame, assume they are teleporting and don't do the collision check.
+        if (Vector3.Distance(lastPosition, currentPosition) > 5f)
+        {
+            lastPosition = transform.position;
+            return;
+        }
+
+        Vector3 direction = currentPosition - lastPosition;
+        direction.y = 0;
+        
+        if (Physics.Raycast(lastPosition, direction, out RaycastHit hit, direction.magnitude, ~ignoreLayers, QueryTriggerInteraction.Ignore))
+        {
+            transform.position = lastPosition;
+        }
+    }
+}
