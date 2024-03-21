2addefd7e2ad503eea520f3572328bc4bd5fa807
microman502 <microman502@gmail.com>
2024-03-21 16:59:38 +1100

    Fixed PlayerCollisionCheck code running for other players

--- Assets/Scripts/Player/PlayerCollisionCheck.cs
+++ Assets/Scripts/Player/PlayerCollisionCheck.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class PlayerCollisionCheck : MonoBehaviour
 {
+    [SerializeField] PhotonView photonView;
     [SerializeField] LayerMask ignoreLayers;
     Vector3 lastPosition;
+
+    void Awake ()
+    {
+        photonView = gameObject.GetComponent<PhotonView>();
+    }
 
     void Start ()
     {
@@ -14,6 +21,9 @@ public class PlayerCollisionCheck : MonoBehaviour
 
     void FixedUpdate ()
     {
+        if (!photonView.IsMine)
+            return;
+
         Vector3 currentPosition = transform.position;
 
         // If the player has moved enough in one physics frame, assume they are teleporting and don't do the collision check.
